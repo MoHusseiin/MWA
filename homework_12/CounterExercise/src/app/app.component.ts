55b3f5bf63461b656897536082bc9069e471a09e
MoHusseiin <mohamedabdeen898@gmail.com>
2019-06-12 01:49:11 -0500

    HomeWork 12

---
+++ homework_12/CounterExercise/src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-counter [parentValue]="8" (componentCounterValue)="getCount1($event)"></app-counter>
+    {{result1}}
+    <br/>
+    <br/>
+
+    <app-counter [parentValue]="8" (componentCounterValue)="getCount2($event)"></app-counter>
+    {{result2}}
+    <br/>
+    <br/>
+
+    <app-counter [parentValue]="8" (componentCounterValue)="getCount3($event)"></app-counter>
+    {{result3}}
+    <br/>
+    <br/>
+
+    <app-counter [parentValue]="8" (componentCounterValue)="getCount4($event)"></app-counter>
+    {{result4}}
+  `,
+  styles: [``]
+})
+export class AppComponent {
+
+  private result1: string;
+  private result2: string;
+  private result3: string;
+  private result4: string;
+
+  getCount1(e: string){
+    this.result1 =  `Component1 Counter Value = ${e}`;
+  }
+
+  getCount2(e: string){
+    this.result2 =  `Component2 Counter Value = ${e}`;
+  }
+
+  getCount3(e: string){
+    this.result3 =  `Component1 Counter Value = ${e}`;
+  }
+
+  getCount4(e: string){
+    this.result4 =  `Component1 Counter Value = ${e}`;
+  }
+}
---
+++ homework_12/CounterExercise/src/app/counter.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template: `
+      <button (click) = decrease()>-</button>
+      {{_countValue}}
+      <button (click) = increase()>+</button>
+      <br/>
+  `,
+  styles: [``]
+})
+export class CounterComponent implements OnInit {
+  
+  private _countValue: number;
+  @Input() parentValue: number;
+  @Output() componentCounterValue: EventEmitter<number>;
+
+  constructor() { 
+    this._countValue = 0;
+    this.componentCounterValue = new EventEmitter();
+  }
+ 
+  ngOnInit() {
+    this._countValue = this.parentValue;
+    this.componentCounterValue.emit(this._countValue);
+  }
+
+  increase(){
+    this._countValue++;
+    this.componentCounterValue.emit(this._countValue);
+    return false;
+  }
+
+  decrease(){
+    this._countValue--;
+    this.componentCounterValue.emit(this._countValue);
+    return false;
+  }
+
+  get CountValue(){
+      return this._countValue;
+  }
+
+  set CountValue(countValue: number){
+    this._countValue = countValue;
+  }
+}
