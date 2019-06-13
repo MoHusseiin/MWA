4dc62d19aa6b89a50e54245a52681bcc4b7f9f06
MoHusseiin <mohamedabdeen898@gmail.com>
2019-06-13 01:13:11 -0500

    HomeWork 13

---
+++ homework_13/DirictivesApp/src/app/Bigger.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, Renderer2, ElementRef, HostBinding, HostListener, OnInit, Input } from '@angular/core';
+
+@Directive({
+    selector: '[makeBigger]'
+})
+export class BiggerDirective implements OnInit{
+
+    @Input('makeBigger') defaultSize: number;
+
+    private elementRef: ElementRef;
+    private renderer2: Renderer2;
+    private myFontSize:number;
+
+    constructor(e: ElementRef, r: Renderer2) {
+        this.elementRef = e;
+        this.renderer2 = r;
+    }
+
+    // @HostBinding('style.font-size.px') myFontSize: number;
+
+    @HostListener('dblclick') onDblClick(){
+        this.myFontSize = +this.myFontSize + 2;
+        console.log("myFontSize => "+this.myFontSize);
+        this.renderer2.setStyle(this.elementRef.nativeElement, "font-size", this.myFontSize+"px");
+    }
+
+    ngOnInit(): void {
+        console.log("defaultSize => "+ this.defaultSize);
+        this.myFontSize = this.defaultSize;
+    }
+}
---
+++ homework_13/DirictivesApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DumpComponent } from './dump.component';
+import { VisiblityDirective } from './visiblity.directive';
+import { BiggerDirective } from './Bigger.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DumpComponent,
+    VisiblityDirective,
+    BiggerDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
